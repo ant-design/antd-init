432f4e94a528f2c3fa4976c327b035ac9d5c6574
sorrycc <sorrycc@gmail.com>
2015-12-18 13:47:07 +0800

    fix: ie8 error with reverse word `default`

--- boilerplate/webpack.config.js
+++ boilerplate/webpack.config.js
@@ -4,6 +4,10 @@ module.exports = function(webpackConfig) {
     if (loader.loader === 'babel') {
       // https://github.com/ant-design/babel-plugin-antd
       loader.query.plugins.push('antd');
+
+      loader.loaders = ['es3ify', 'babel?'+JSON.stringify(loader.query)];
+      delete loader.query;
+      delete loader.loader;
     }
     return loader;
   });
