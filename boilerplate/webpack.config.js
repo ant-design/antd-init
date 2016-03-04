07ebe3eb64e04ff4d92d3214e3f0ec37cb85aaff
sorrycc <sorrycc@gmail.com>
2016-03-04 14:53:19 +0800

    update deps

--- boilerplate/webpack.config.js
+++ boilerplate/webpack.config.js
@@ -1,17 +1,5 @@
+
 module.exports = function(webpackConfig) {
-  webpackConfig.module.loaders.forEach(function(loader) {
-    if (loader.loader === 'babel') {
-      // https://github.com/ant-design/babel-plugin-antd
-      loader.query.plugins.push('antd');
-    }
-    return loader;
-  });
-
-  // Fix ie8 compatibility
-  webpackConfig.module.loaders.unshift({
-    test: /\.jsx?$/,
-    loader: 'es3ify',
-  });
-
+  webpackConfig.babel.plugins.push('antd');
   return webpackConfig;
 };
