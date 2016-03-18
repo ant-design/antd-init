164a8d1b36ab19c4f54cde647a7322efe62d5491
sorrycc <sorrycc@gmail.com>
2016-03-18 17:47:37 +0800

    revert: remove es3ify-loader, Fix https://github.com/ant-design/ant-design/issues/1217

--- boilerplate/webpack.config.js
+++ boilerplate/webpack.config.js
@@ -1,4 +1,11 @@
 module.exports = function(webpackConfig) {
   webpackConfig.babel.plugins.push('antd');
+
+  // Fix ie8 compatibility
+  webpackConfig.module.loaders.unshift({
+    test: /\.jsx?$/,
+    loader: 'es3ify-loader',
+  });
+
   return webpackConfig;
 };
