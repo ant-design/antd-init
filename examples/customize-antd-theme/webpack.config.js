b2c66824205c736a9f84bc945890aa1a96504570
afc163 <afc163@gmail.com>
2016-04-03 15:36:38 +0800

    upgrade deps

--- examples/customize-antd-theme/webpack.config.js
+++ examples/customize-antd-theme/webpack.config.js
@@ -1,10 +1,10 @@
 module.exports = function(webpackConfig) {
-  webpackConfig.module.loaders.forEach(function(loader) {
-    if (loader.loader === 'babel') {
-      // https://github.com/ant-design/babel-plugin-antd
-      loader.query.plugins.push('antd');
-    }
-    return loader;
+  webpackConfig.babel.plugins.push('antd');
+
+  // Fix ie8 compatibility
+  webpackConfig.module.loaders.unshift({
+    test: /\.jsx?$/,
+    loader: 'es3ify-loader',
   });
 
   return webpackConfig;
