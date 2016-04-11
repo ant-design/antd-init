d50a0ef3125edddecd090e7edd9af726cd6d5c42
sorrycc <sorrycc@gmail.com>
2016-04-11 16:55:10 +0800

    add antd plugin

--- boilerplates/redux/webpack.config.js
+++ boilerplates/redux/webpack.config.js
@@ -3,6 +3,14 @@
 const webpack = require('atool-build/lib/webpack');
 
 module.exports = function(webpackConfig) {
+  webpackConfig.babel.plugins.push('antd');
   webpackConfig.babel.plugins.push('jsx-control-statements');
+
+  // Fix ie8 compatibility
+  //webpackConfig.module.loaders.unshift({
+  //  test: /\.jsx?$/,
+  //  loader: 'es3ify-loader',
+  //});
+
   return webpackConfig;
 };
