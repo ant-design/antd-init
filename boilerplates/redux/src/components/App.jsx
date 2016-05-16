601dacdc21e3dd246c530f20d5b0dcf13f5022d3
sorrycc <sorrycc@gmail.com>
2016-05-16 18:21:07 +0800

    use App as router entry

--- boilerplates/redux/src/components/App.jsx
+++ boilerplates/redux/src/components/App.jsx
@@ -1,10 +1,13 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import Todos from './Todos/Todos';
+import Layout from './Layout';
 
 const App = ({ location }) => {
   return (
-    <div><Todos location={location}/></div>
+    <Layout>
+      <Todos location={location} />
+    </Layout>
   );
 };
 
