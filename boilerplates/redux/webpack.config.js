91f8e52d44d7ddc8ba712ee8afc23ddf933ed06e
sorrycc <sorrycc@gmail.com>
2016-05-17 15:37:40 +0800

    code style

--- boilerplates/redux/webpack.config.js
+++ boilerplates/redux/webpack.config.js
@@ -1,15 +1,16 @@
-// More Info: https://github.com/ant-tool/atool-build#配置扩展
+// Learn more on how to config.
+// - https://github.com/ant-tool/atool-build#配置扩展
 
 const webpack = require('atool-build/lib/webpack');
 
 module.exports = function(webpackConfig) {
   webpackConfig.babel.plugins.push(['antd', {style: 'css'}]);
 
-  // Fix ie8 compatibility
-  //webpackConfig.module.loaders.unshift({
-  //  test: /\.jsx?$/,
-  //  loader: 'es3ify-loader',
-  //});
+  // Enable this if you have to support IE8
+  // webpackConfig.module.loaders.unshift({
+  //   test: /\.jsx?$/,
+  //   loader: 'es3ify-loader',
+  // });
 
   webpackConfig.module.loaders.forEach(function(loader, index) {
     if (typeof loader.test === 'function' && loader.test.toString().indexOf('\\.less$') > -1) {
