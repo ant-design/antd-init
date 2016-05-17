edec56ce02df07fb7ca104ff0e771fc749fdaf5d
sorrycc <sorrycc@gmail.com>
2016-05-17 15:23:35 +0800

    support saga hmr

---
+++ boilerplates/redux/src/sagas/SagaManager.js
@@ -0,0 +1,31 @@
+import rootSaga from './index';
+import { take, fork, cancel } from 'redux-saga/effects';
+
+const sagas = [rootSaga];
+
+export const CANCEL_SAGAS_HMR = 'CANCEL_SAGAS_HMR';
+
+function createAbortableSaga (saga) {
+  if (process.env.NODE_ENV === 'development') {
+    return function* main () {
+      const sagaTask = yield fork(saga);
+      yield take(CANCEL_SAGAS_HMR);
+      yield cancel(sagaTask);
+    };
+  } else {
+    return saga;
+  }
+}
+
+const SagaManager = {
+  startSagas(sagaMiddleware) {
+    sagas.map(createAbortableSaga).forEach(saga => sagaMiddleware.run(saga));
+  },
+  cancelSagas(store) {
+    store.dispatch({
+      type: CANCEL_SAGAS_HMR
+    });
+  }
+};
+
+export default SagaManager;
--- boilerplates/redux/src/sagas/index.js
+++ boilerplates/redux/src/sagas/index.js
@@ -3,7 +3,7 @@ import { fork } from 'redux-saga/effects';
 // Use require.context to require sagas automatically
 // Ref: https://webpack.github.io/docs/context.html
 const context = require.context('./', false, /\.js$/);
-const keys = context.keys().filter(item => item !== './index.js');
+const keys = context.keys().filter(item => item !== './index.js' && item !== './SagaManager.js');
 
 export default function* root() {
   for (let i = 0; i < keys.length; i ++) {
