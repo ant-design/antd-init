b8e04f639a65fe4a78c3a398b0bf8ed2c613244f
sorrycc <sorrycc@gmail.com>
2016-05-17 17:19:36 +0800

    Load src/entries/*.js as entry automatically

--- boilerplates/redux/webpack.config.js
+++ boilerplates/redux/webpack.config.js
@@ -2,16 +2,20 @@
 // - https://github.com/ant-tool/atool-build#配置扩展
 
 const webpack = require('atool-build/lib/webpack');
+const fs = require('fs');
+const path = require('path');
+const glob = require('glob');
 
 module.exports = function(webpackConfig) {
   webpackConfig.babel.plugins.push(['antd', {style: 'css'}]);
 
-  // Enable this if you have to support IE8
+  // Enable this if you have to support IE8.
   // webpackConfig.module.loaders.unshift({
   //   test: /\.jsx?$/,
   //   loader: 'es3ify-loader',
   // });
 
+  // Parse all less files as css module.
   webpackConfig.module.loaders.forEach(function(loader, index) {
     if (typeof loader.test === 'function' && loader.test.toString().indexOf('\\.less$') > -1) {
       loader.test = /\.dont\.exist\.file/;
@@ -20,6 +24,15 @@ module.exports = function(webpackConfig) {
       loader.test = /\.less$/;
     }
   });
+
+  // Load src/entries/*.js as entry automatically.
+  const files = glob.sync('./src/entries/*.js');
+  const newEntries = files.reduce(function(memo, file) {
+    const name = path.basename(file, '.js');
+    memo[name] = file;
+    return memo;
+  }, {});
+  webpackConfig.entry = Object.assign({}, webpackConfig.entry, newEntries);
 
   return webpackConfig;
 };
