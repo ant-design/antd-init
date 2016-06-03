d5ea2188c08291d0c31707d15fc399a95efabb7a
sorrycc <sorrycc@gmail.com>
2016-06-03 16:21:26 +0800

    rename api url

--- boilerplates/plain-react/src/services/tableList.js
+++ boilerplates/plain-react/src/services/tableList.js
@@ -3,26 +3,26 @@ import xFetch from './xFetch';
 import qs from 'qs';
 
 export async function getData(params) {
-  // You can replace xFetch to other ajax library
+  // You can replace xFetch to other ajax libraries
   return xFetch(`/api/tablelist?${qs.stringify(params)}`);
 }
 
 export async function addItem(params) {
-  return xFetch('/api/tablelist_addItem', {
+  return xFetch('/api/tablelist/create', {
     method: 'post',
     body: qs.stringify(params),
   });
 }
 
 export async function deleteItem(params) {
-  return xFetch('/api/tablelist_deleteItem', {
+  return xFetch('/api/tablelist/delete', {
     method: 'post',
     body: qs.stringify(params),
   });
 }
 
 export async function editItem(params) {
-  return xFetch('/api/tablelist_editItem', {
+  return xFetch('/api/tablelist/edit', {
     method: 'post',
     body: qs.stringify(params),
   });
