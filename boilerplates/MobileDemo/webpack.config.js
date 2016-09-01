2d0dec0ca382095857807a27b4f09bb4ada45e2c
yiminghe <yiminghe@gmail.com>
2016-09-01 16:59:22 +0800

    update to antd-mobile 0.8.x

--- boilerplates/MobileDemo/webpack.config.js
+++ boilerplates/MobileDemo/webpack.config.js
@@ -2,6 +2,7 @@
 // - https://github.com/ant-tool/atool-build#配置扩展
 
 const webpack = require('atool-build/lib/webpack');
+const pxtorem = require('postcss-pxtorem');
 
 module.exports = function (webpackConfig) {
   webpackConfig.babel.plugins.push('transform-runtime');
@@ -9,5 +10,9 @@ module.exports = function (webpackConfig) {
     style: 'css',  // if true, use less
     libraryName: 'antd-mobile',
   }]);
+  webpackConfig.postcss.push(pxtorem({
+    rootValue: 100,
+    propWhiteList: [],
+  }));
   return webpackConfig;
 };
