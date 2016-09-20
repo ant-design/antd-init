5d238415758b1b7747c80d8f55abaaa8c7deadf3
sorrycc <sorrycc@gmail.com>
2016-09-20 11:30:46 +0800

    use babel-plugin-import

--- boilerplates/demo/webpack.config.js
+++ boilerplates/demo/webpack.config.js
@@ -1,7 +1,8 @@
 
 module.exports = function(webpackConfig) {
   webpackConfig.babel.plugins.push('transform-runtime');
-  webpackConfig.babel.plugins.push(['antd', {
+  webpackConfig.babel.plugins.push(['import', {
+    libraryName: 'antd',
     style: 'css',
   }]);
 
