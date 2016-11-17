1b52b179b99c1485a3d59e1d52b9be6dbe2dca96
afc163 <afc163@gmail.com>
2016-11-17 16:00:49 +0800

    update

--- boilerplates/demo/webpack.config.js
+++ boilerplates/demo/webpack.config.js
@@ -1,3 +1,5 @@
+// Learn more on how to config.
+// - https://github.com/ant-tool/atool-build#配置扩展
 
 module.exports = function(webpackConfig) {
   webpackConfig.babel.plugins.push('transform-runtime');
